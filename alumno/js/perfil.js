60e1d7a4a1bdfef3936b78c067b9e757ac586abf
Xitlali Valenzo <168685289+XitlaliValenzo@users.noreply.github.com>
2024-11-11 08:53:03 -0600

    Delete alumno directory

--- alumno/js/perfil.js
+++
@@ -1,7 +0,0 @@
-function editarContrasena() {
-  var nuevaContraseña = prompt("Introduce tu nueva contraseña:");
-  if (nuevaContraseña != null) {
-    // Aquí puedes enviar la nueva contraseña al servidor utilizando AJAX o alguna otra técnica
-    alert("Contraseña actualizada correctamente");
-  }
-}
